0377cd6f46cca9f0f2f1e68ea33d699f421cb5f5
Eric Oligney <EOengineer@gmail.com>
2018-09-18 12:40:34 -0400

    user creation and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
+  get    '/login',   to: 'sessions#new', as: 'new_session'
+  post   '/login',   to: 'sessions#create', as: 'sessions'
+  get    '/logout',  to: 'sessions#destroy', as: 'destroy_session'
+
+  resources :users, only: [:new, :create]
+
   namespace :admin do
     root to: "home#index"
     get    '/login',   to: 'sessions#new', as: 'new_session'
